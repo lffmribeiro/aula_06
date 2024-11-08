75cde95294af1f7cb0a3633a0ac5ada0ddeba570
Luiz Fernando Ribeiro <fernandolmessias@hotmail.com>
2024-11-08 14:22:51 -0300

    cricao do exercicio data

---
+++ data.py
@@ -0,0 +1,17 @@
+# digitar a data e imprimir separadamente
+from datetime import date
+
+def pedir_data():
+    try:
+        data = input('Digite uma data no formato dd/mm/yyyy: ')
+        lista = data.split('/')
+        dia = int(lista[0])
+        mes = int(lista[1])
+        ano = int(lista[2])
+        data = date(day=dia, month=mes, year=ano)
+        print(f'São Paulo, dia {dia} do mês {mes} do ano {ano}, ou seja, sua data é {data}.')
+    except (ValueError, IndexError):
+        print('Tente novamente')
+        pedir_data()
+    
+pedir_data()
