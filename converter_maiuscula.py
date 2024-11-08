228db8380130f9b254bbce87c235315ae228d00a
Luiz Fernando Ribeiro <fernandolmessias@hotmail.com>
2024-11-08 14:25:41 -0300

    cricao de exercico para maiuscula

---
+++ converter_maiuscula.py
@@ -0,0 +1,8 @@
+#converter em letra maiuscula
+
+def solicitar_nome_usuario():
+    print('*******Converter Nome *********')
+    nome = input("digite o nome: ")
+    print(nome.upper())
+
+solicitar_nome_usuario()
